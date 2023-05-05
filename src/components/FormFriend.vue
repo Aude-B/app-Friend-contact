957ecb2b0ce53f348261957a12e001c8c89695a1
Aude <beclin.aude@gmail.com>
2023-05-05 18:42:29 +0200

    app done

---
+++ src/components/FormFriend.vue
@@ -0,0 +1,44 @@
+<template>
+  form@-----------------------------------------------
+  <form>
+    @submit.prevent="
+    <div>
+      <label for="name">FullName</label>
+      <input type="text" name="name" />
+    </div>
+
+    <div>
+      <label for="address">Address</label>
+      <input type="text" name="address" />
+    </div>
+
+    <div>
+      <label for="email">Email</label>
+      <input type="text" name="email" />
+    </div>
+
+    <div>
+      <label for="phone">Phone</label>
+      <input type="text" name="phone" />
+    </div>
+
+    <input type="submit" value="Create a new friend" />
+  </form>
+</template>
+
+<script>
+export default {
+  props: ["creatFriend"],
+  data() {
+    return {
+      image: " ",
+      name: " ",
+      phone: " ",
+      email: " ",
+      adresse: " ",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/FriendItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <li>
+     <img :src="image" :alt="name"/>   
+     <h2>
+        {{ name }}
+         </h2>
+         <button>Show Détails</button>
+<ul>
+    <li>adress : {{adress}}</li>
+    <li>Phone :{{Phone}}</li>
+    <li>Email : {{Email}}</li>
+</ul>
+
+</template>
